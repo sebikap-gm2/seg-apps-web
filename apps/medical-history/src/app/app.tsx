7ff47e78dacae8beecf92b259e61907635b0e6fb
nahuehernandez <nahueldahernandez@gmail.com>
2023-10-08 13:32:51 -0300

    agrego posible pantalla de perfil

--- apps/medical-history/src/app/app.tsx
+++ apps/medical-history/src/app/app.tsx
@@ -3,6 +3,7 @@ import { Message } from '@seg-apps-web/api-interfaces';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from '../views/login/login';
 import Home from '../views/home/home';
+import Profile from '../views/profile/profile';
 
 export const App = () => {
   const [m, setMessage] = useState<Message>({ message: '' });
@@ -18,6 +19,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Login/>} />
         <Route path="/home" element={<Home/>} />
+        <Route path="/profile" element={<Profile/>} />
         {/*<Route element={<NotFound/>} />*/}
       </Routes>
     </Router>
