385dcd4d0965cb42e715fa3310163b6dd2835e51
sebikap-gm2 <sebastiankaplanski@gmail.com>
2023-10-16 11:13:56 -0300

    feat: integrate login

--- apps/medical-history/src/app/app.tsx
+++ apps/medical-history/src/app/app.tsx
@@ -1,5 +1,3 @@
-import React, { useEffect, useState } from 'react';
-import { Message } from '@seg-apps-web/api-interfaces';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from '../views/login/login';
 import Home from '../views/home/home';
@@ -7,26 +5,8 @@ import Profile from '../views/profile/profile';
 import ForgotPassword from '../views/forgotPassword/forgotPassword';
 import Register from '../views/register/Register';
 import NotFound from '../views/notfound/notfound';
-import { HTTP } from '../services';
-const http = new HTTP();
 
 export const App = () => {
-  const [m, setMessage] = useState<Message>({ message: '' });
-
-  useEffect(() => {
-    fetch('/api')
-      .then((r) => r.json())
-      .then(setMessage);
-  }, []);
-
-  const handleClick = async () => {
-    const res = await http.post('http://localhost:3333/login', {
-      username: 'admin',
-      password: 'admin123',
-    });
-    setMessage(res);
-  };
-
   return (
     <Router>
       <Routes>
--- apps/medical-history/src/views/login/login.tsx
+++ apps/medical-history/src/views/login/login.tsx
@@ -1,89 +1,76 @@
-import React, { useEffect, useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import './login-styles.css';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { HTTP } from '../../services';
+
+const http = new HTTP('http://localhost:3333');
 
 const Login = () => {
-  /*const [m, setMessage] = useState<Message>({ message: '' });
-  useEffect(() => {
-    fetch('/api')
-      .then((r) => r.json())
-      .then(setMessage);
-  }, []);*/
-
   const navigate = useNavigate();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("")
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [warningMessage, setWarningMessage] = useState('');
 
+  const logIn = async () => {
+    const res = await http.post('/login', { username, password });
+    console.log(res);
 
-  const logIn = () => {
-
-    //pegarle con el user y la pass la back.
-
-    // Si las credenciales son incorrectas, establece el mensaje de advertencia.
-    if (username === 'admin') {
-      if (password === 'admin') {
-        console.log("user: " + username);
-        console.log("pass: " + password)
-        navigate('/home');
-      } else {
-        setWarningMessage('Contraseña incorrecta');
-      }
-    } else {
-      setWarningMessage('Ese usuario no existe');
+    if (res.ok) {
+      navigate('/home');
+      return;
     }
+    setWarningMessage(res.payload.message);
   };
 
+  const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) =>
+    setUsername(event.target.value);
 
-  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(event.target.value); // Actualiza username con el valor del campo de entrada
-  };
-
-
-  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value); // Actualiza username con el valor del campo de entrada
-  };
-
-
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) =>
+    setPassword(event.target.value);
 
   return (
-    <>
-      <div className="wrapper fadeInDown">
-        <div id="formContent">
-          <div className="logo">
-            <img src="../assets/Medicare.png" alt="Logo" />
-          </div>
-          <input
-            type="text"
-            id="username"
-            className="fadeIn second"
-            name="login"
-            placeholder="Usuario"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-          <input
-            type="password"
-            id="password"
-            className="fadeIn third"
-            name="login"
-            placeholder="Contraseña"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-          <input type="submit" onClick={logIn} className="fadeIn fourth" value="Ingresar" />
-          {warningMessage && <div className="warning">{warningMessage}</div>}
-          <div id="formFooter">
-            <a onClick={() => navigate('/forgotPassword')} className="underlineHover">¿Olvidaste tu Contraseña?</a>
-            <span className="separator">|</span>
-            <a onClick={() => navigate('/register')} className="underlineHover">Registrarse</a>
-          </div>
-
+    <div className="wrapper fadeInDown">
+      <div id="formContent">
+        <div className="logo">
+          <img src="../assets/Medicare.png" alt="Logo" />
+        </div>
+        <input
+          type="text"
+          id="username"
+          className="fadeIn second"
+          name="login"
+          placeholder="Usuario"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+        <input
+          type="password"
+          id="password"
+          className="fadeIn third"
+          name="login"
+          placeholder="Contraseña"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <input
+          type="submit"
+          onClick={logIn}
+          className="fadeIn fourth"
+          value="Ingresar"
+        />
+        {warningMessage && <div className="warning">{warningMessage}</div>}
+        <div id="formFooter">
+          <Link to="/forgotPassword" className="underlineHover">
+            ¿Olvidaste tu Contraseña?
+          </Link>
+          <span className="separator">|</span>
+          <Link to="/register" className="underlineHover">
+            Registrarse
+          </Link>
         </div>
       </div>
-    </>
+    </div>
   );
-}
-
+};
 
 export default Login;
